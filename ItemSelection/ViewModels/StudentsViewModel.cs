5008fddd71b3689eb75d00e67029cb77653bbcad
Demon6590 <dima.chernytin@gmail.com>
2026-06-11 15:23:31 +0300

    сделал отоброжение в кнопках

--- ItemSelection/ViewModels/StudentsViewModel.cs
+++ ItemSelection/ViewModels/StudentsViewModel.cs
@@ -10,6 +10,9 @@ public class StudentsViewModel : ViewModelBase
 
      public StudentsViewModel()
      {
+         App.Students.Add(new Student("Иванов", "Иван", "Иванович", ["Математика", "Физика", "Информатика"]));
+         App.Students.Add(new Student("Петров", "Петр", "Петрович", ["История", "Философия"]));
+         App.Students.Add(new Student("Сидорова", "Анна", "Сергеевна", ["Химия", "Биология", "Английский"]));
          Students.Add(new Student("Иванов", "Иван", "Иванович", ["Математика", "Физика", "Информатика"]));
          Students.Add(new Student("Петров", "Петр", "Петрович", ["История", "Философия"]));
          Students.Add(new Student("Сидорова", "Анна", "Сергеевна", ["Химия", "Биология", "Английский"]));
